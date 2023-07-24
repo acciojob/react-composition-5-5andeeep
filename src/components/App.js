a9c88e279b20d0ea81e0b0bcdb56c3609e0f6390
Sandeep <newsandeep03@gmail.com>
2023-07-24 11:39:50 +0530

    ans added second attempt

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,14 @@ import Tabs from "./Tabs";
 const App = () => {
 
   const tabsSet1 =[
-    {title:"Tab 1", content: "Component for Tab 1"},
-    {title:"Tab 2", content: "Component for Tab 2"},
-    {title:"Tab 3", content: "Component for Tab 3"},
+    {title:"Tab 1", content: "Content for Tab 1"},
+    {title:"Tab 2", content: "Content for Tab 2"},
+    {title:"Tab 3", content: "Content for Tab 3"},
   ]
   const tabsSet2 =[
-    {title:"Tab A", content: "Component for Tab A"},
-    {title:"Tab B", content: "Component for Tab B"},
-    {title:"Tab C", content: "Component for Tab C"},
+    {title:"Tab A", content: "Content for Tab A"},
+    {title:"Tab B", content: "Content for Tab B"},
+    {title:"Tab C", content: "Content for Tab C"},
   ]
 
   return (
